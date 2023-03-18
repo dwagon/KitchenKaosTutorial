4ad9482196587eeeedadafef1ce0d362fe1915b9
Dougal Scott <Dougal.Scott@gmail.com>
2023-03-18 18:19:19 +1100

    rotate player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	[SerializeField] private float moveSpeed = 7.0f;
+	[SerializeField] private float rotateSpeed = 10.0f;
 
     private void Update() {
 		Vector2 inputVector = new Vector2(0, 0);
@@ -23,5 +24,6 @@ public class Player : MonoBehaviour
 		inputVector = inputVector.normalized;
 		Vector3 moveDir = new Vector3(inputVector.x, 0.0f, inputVector.y);
 		transform.position += moveDir * moveSpeed * Time.deltaTime;
+		transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 	}
 }
