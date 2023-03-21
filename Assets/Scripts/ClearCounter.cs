68e52a51f5d4cc1443d7149693f2aeb38f362f6b
Dougal Scott <Dougal.Scott@gmail.com>
2023-03-21 16:29:03 +1100

    More counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,6 +6,21 @@ public class ClearCounter : BaseCounter
 {
 	[SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-	public override void Interact(Player player) {
+	public override void Interact(Player player)
+	{
+		if (!HasKitchenObject()) {
+			// No kitchen object on counter
+			if (player.HasKitchenObject()) {
+				player.GetKitchenObject().SetKitchenObjectParent(this);
+			}
+		} else {
+			// Kitchen obj present on counter
+			if (player.HasKitchenObject()) {
+				// Player is already carrying something
+			}
+			else {
+				GetKitchenObject().SetKitchenObjectParent(player);
+			}
+		}
 	}
 }
