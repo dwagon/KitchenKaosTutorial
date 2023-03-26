6c41bbcfb463fb152f3964f9e164fb433c164528
Dougal Scott <Dougal.Scott@gmail.com>
2023-03-26 15:45:25 +1100

    Completed plate visual

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -2,25 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : BaseCounter
-{
-	[SerializeField] private KitchenObjectSO kitchenObjectSO;
+public class ClearCounter : BaseCounter {
 
-	public override void Interact(Player player)
-	{
-		if (!HasKitchenObject()) {
-			// No kitchen object on counter
-			if (player.HasKitchenObject()) {
-				player.GetKitchenObject().SetKitchenObjectParent(this);
-			}
-		} else {
-			// Kitchen obj present on counter
-			if (player.HasKitchenObject()) {
-				// Player is already carrying something
-			}
-			else {
-				GetKitchenObject().SetKitchenObjectParent(player);
-			}
-		}
-	}
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+
+    public override void Interact(Player player) {
+        if (!HasKitchenObject()) {
+            // There is no KitchenObject here
+            if (player.HasKitchenObject()) {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+                // Player not carrying anything
+            }
+        } else {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject()) {
+                // Player is carrying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    // Player is holding a Plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())) {
+                        GetKitchenObject().DestroySelf();
+                    }
+                } else {
+                    // Player is not carrying Plate but something else
+                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject)) {
+                        // Counter is holding a Plate
+                        if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO())) {
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
+            } else {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateKitchenObject : KitchenObject
+{
+	public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+	public class OnIngredientAddedEventArgs: EventArgs {
+		public KitchenObjectSO kitchenObjectSO;
+	}
+	[SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+
+	public List<KitchenObjectSO> kitchenObjectSOList;
+
+	public void Awake()
+	{
+		kitchenObjectSOList = new List<KitchenObjectSO>();
+	}
+
+	public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO) {
+		if (!validKitchenObjectSOList.Contains(kitchenObjectSO)) {
+			// Not a plateable item
+			return false;
+		}
+		if(kitchenObjectSOList.Contains(kitchenObjectSO)) {
+			// Already contains this type
+			return false;
+		}
+		kitchenObjectSOList.Add(kitchenObjectSO);
+
+		OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {
+			kitchenObjectSO = kitchenObjectSO
+		});
+		return true;
+	}
+}
